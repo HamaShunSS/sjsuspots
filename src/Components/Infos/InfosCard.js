1167a4849b7fd09d6c4cbaa5c7b50422e85692cb
Shunsuke Hama <shunsuke.hama@gmail.com>
2019-04-21 22:56:07 -0700

    v3

--- src/Components/Infos/InfosCard.js
+++ src/Components/Infos/InfosCard.js
@@ -9,7 +9,7 @@ const Card = ({ info } ) => {
         <div className='tc bg-light-green dib br3 pa4 ma3 grow bw2 shadow-5 w5-ns'>
             <div className="black">
                 <h2>{info.name}</h2>
-                <img id='inputimage' alt='画像' src={info.url} className="pv3 fl w4 h4 h4-ns w4-ns tc" />
+                <img id='inputimage' alt='画像' src={info.url} className="pv3 fl w5 h4 h4-ns w5-ns tc" />
                 <p className="">住所：{info.location}</p>
                 <p>星：{info.rate}</p>
                 <p>コメント：{info.comments}</p>
